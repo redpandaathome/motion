c079bbcff96755280b61085b9be095cd6ba4ff18
redpandaathome <leeyumi415@gmail.com>
2021-04-07 12:40:12 +0900

    Updates

--- app.b3a8c8ec.js
+++ app.b3a8c8ec.js
@@ -880,7 +880,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65220" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50979" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
